5875eaffdcfe64bf915e28e8444815d67c0d9ad3
Lautaro1387 <5134@holbertonschool.com>
2022-08-17 07:04:03 -0700

    Update main

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 	(void)argc;
 	if (argc < 1)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(buffer, 1024, fp))
