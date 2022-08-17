3dbceaf23577b46a0a6e60700ab50397fad2f828
Lautaro1387 <5134@holbertonschool.com>
2022-08-17 06:57:09 -0700

    Update opcodes

--- opcodes.c
+++ opcodes.c
@@ -61,7 +61,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack) || !stack)
 	{
-		dprintf("L%u: can't pint, stack empty\n", line_number);
+		dprintf(STDERR_FILENO, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
