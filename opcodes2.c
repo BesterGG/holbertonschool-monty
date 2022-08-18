0d8e7cff0db080bd982e0def8407c9eb79999135
Lautaro1387 <5134@holbertonschool.com>
2022-08-17 19:40:10 -0700

    Fix opcodes

---
+++ opcodes2.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head, *new_node;
+
+	head = *stack;
+	if (head == NULL || head->next == NULL)
+	{
+	dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	new_node = head->n;
+	head->n = head->next->n;
+	head->next->n = new_node;
+}
+void _add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack, *new_stack;
+
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new_stack = head->n + head->next->n;
+	head->n = new_stack;
+}
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	line_number = line_number;
+	stack = stack;
+}
