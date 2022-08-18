60eab80e9ff4cb8c4e56d13e4ed68476efda12b3
Lautaro1387 <5134@holbertonschool.com>
2022-08-18 09:39:08 -0700

    Change opcodes

--- main.c
+++ main.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 	(void)argc;
 	if (argc < 1)
 	{
-		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(buffer, 1024, fp))
